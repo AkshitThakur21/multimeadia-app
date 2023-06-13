5ad856437660508ea050f60990c9d5407e940400
Akshit Thakur <80190003+AkshitThakur21@users.noreply.github.com>
2023-06-13 12:30:53 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 1,
+    name: 'Video File',
+    type: 'video',
+    path: "/file-server/v1.mp4"
+  },
+  {
+    id: 2,
+    name: 'Audio File',
+    type: 'audio',
+    path: "/file-server/a1.mp3"
+  },
+  {
+    id: 3,
+    name: 'Document File',
+    type: 'document',
+    path: "/file-server/d1.pdf"
+  },
+  {
+    id: 4,
+    name: 'Image File',
+    type: 'image',
+    path: "/file-server/p1.jpg"
+  },
+    {
+    id: 5,
+    name: 'Image File',
+    type: 'image',
+    path: "/file-server/p5.jpg"
+  },
+  {
+    id: 6,
+    name: 'Image File',
+    type: 'image',
+    path: "/file-server/p6.jpg"
+  },
+]
